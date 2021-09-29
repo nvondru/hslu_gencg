039665f1a40e1c0f832318470d7ddfff2466ed7a
Nicolas Vondru <niki.vondru@hotmail.ch>
2021-09-30 00:11:43 +0200

    try to make iframes height fit to content

--- resizeIframes.js
+++ resizeIframes.js
@@ -1,27 +1,27 @@
-function resizeIFrameToFitContent( iFrame ) {
-
-    let bodyIFrame = iFrame.contentWindow.document.getElementsByTagName("body")[0];
-    let htmlIFrame = iFrame.contentWindow.document.getElementsByTagName("html")[0];
-    iFrame.width  = bodyIFrame.scrollWidth;
-    iFrame.height = Math.max( bodyIFrame.scrollHeight, bodyIFrame.offsetHeight, 
-        htmlIFrame.clientHeight, htmlIFrame.scrollHeight, htmlIFrame.offsetHeight )
+function resizeIFrameToFitContent(iFrame) {
+  let bodyIFrame =
+    iFrame.contentWindow.document.getElementsByTagName("body")[0];
+  let htmlIFrame =
+    iFrame.contentWindow.document.getElementsByTagName("html")[0];
+  // iFrame.width  = bodyIFrame.scrollWidth;
+  iFrame.height = Math.max(
+    bodyIFrame.scrollHeight,
+    bodyIFrame.offsetHeight,
+    htmlIFrame.clientHeight,
+    htmlIFrame.scrollHeight,
+    htmlIFrame.offsetHeight
+  );
 }
 
-
-
-
 window.addEventListener("DOMContentLoaded", () => {
-
-    setTimeout(() => {
-        
-        // or, to resize all iframes:
-        var iframes = document.querySelectorAll("iframe");
-        for( var i = 0; i < iframes.length; i++) {
-            let frame = iframes[i];
-            frame.addEventListener("load", () => {
-                
-                resizeIFrameToFitContent( frame );
-            })
-        }
-    }, 0);
+  setTimeout(() => {
+    // or, to resize all iframes:
+    var iframes = document.querySelectorAll("iframe");
+    for (var i = 0; i < iframes.length; i++) {
+      let frame = iframes[i];
+      frame.addEventListener("load", () => {
+        resizeIFrameToFitContent(frame);
+      });
+    }
+  }, 0);
 });
